4d97ffba8c4bf2b06f007a06a95606caab1c3fff
noahsark13 <nhk3466@rit.edu>
2024-10-17 20:59:35 -0400

    paint

---
+++ Assets/OurScripts/DoorHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHandler : MonoBehaviour
+{
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
