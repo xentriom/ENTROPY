3208a1a0ee61b3355cc42f652d1429abe954e080
noahsark13 <nhk3466@rit.edu>
2024-11-01 03:13:04 -0400

    Door adjustments

--- Assets/OurScripts/DoorHandler.cs
+++ Assets/OurScripts/DoorHandler.cs
@@ -7,11 +7,22 @@ using UnityEngine.Jobs;
 
 public class DoorHandler : MonoBehaviour
 {
+
+    public enum States
+    {
+        Closed,
+        Closing,
+        Open,
+        Opening
+    }
+
+
     public GameObject DoorUI = null;
     public bool dialogueComplete = false;
-    private bool isOpen = false;
     private bool inArea = false;
   
+
+    public States states = States.Closed;
 
     public float speed = 1.0f;
     private Transform current;
@@ -35,7 +46,7 @@ public class DoorHandler : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
       
-        if (!isOpen && other.tag == "Player")
+        if (states == States.Closed && other.tag == "Player")
         {
             inArea = true;
         }
@@ -55,28 +66,56 @@ public class DoorHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+        // opens ui if player has been sitting with the trigger box
         if (dialogueComplete && inArea)
         {
             DoorUI.SetActive(true);
         }
 
-        if (isOpen)
+        // handles different door interactions
+        switch(states)
         {
-            if (transform.position != target.position)
-            {
-                sinTime += Time.deltaTime * speed;
-                sinTime = Mathf.Clamp(sinTime, 0, Mathf.PI);
-                // sin function
-                float t = 0.5f * Mathf.Sin(sinTime - Mathf.PI / 2f) + 0.5f;
-                transform.position = Vector3.Lerp(current.position, target.position, t);
-            }
-            else
-            {
-                // future update to open and close the door
-                // this keeps track of when the door has fully opened to change a variable
-                //openDoor = false;
-            }
+            case States.Opening:
+                {
+                    if (transform.position != target.position)
+                    {
+                        sinTime += Time.deltaTime * speed;
+                        sinTime = Mathf.Clamp(sinTime, 0, Mathf.PI);
+                        // sin function
+                        float t = 0.5f * Mathf.Sin(sinTime - Mathf.PI / 2f) + 0.5f;
+                        transform.position = Vector3.Lerp(current.position, target.position, t);
+                    }
+                    else
+                    {
+                        states = States.Open;
+                    }
+
+
+                    break;
+                }
+
+            case States.Closing:
+                {
+                    if (transform.position != target.position)
+                    {
+                        sinTime += Time.deltaTime * speed;
+                        sinTime = Mathf.Clamp(sinTime, 0, Mathf.PI);
+                        // sin function
+                        float t = 0.5f * Mathf.Sin(sinTime - Mathf.PI / 2f) + 0.5f;
+                        transform.position = Vector3.Lerp(current.position, target.position, t);
+                    }
+                    else
+                    {
+                        states = States.Open;
+                    }
+
+                    break;
+                }
+
         }
+
 
     }
 
@@ -86,7 +125,7 @@ public class DoorHandler : MonoBehaviour
         // if UI is active you can press button
         if(DoorUI.activeInHierarchy)
         {
-            isOpen = true;
+            states = States.Opening;
 
         }
     }
